5e725b417e62d4b14218b613dd2174af322e6d12
booblik2022 <booblik2022@gmail.com>
2024-01-30 17:08:36 +0300

    Подключил свайпер программ

--- source/js/modules/init-swipers.js
+++ source/js/modules/init-swipers.js
@@ -1,11 +1,13 @@
 import {initSliderHero} from './hero-swiper';
 import {initSliderReviews} from './reviews-swiper';
 import {initSliderNews} from './news-swiper';
+import {initSliderPrograms} from './programs-swiper';
 
 const initSliders = () => {
   initSliderHero();
   initSliderReviews();
   initSliderNews();
+  //initSliderPrograms();
 };
 
 
