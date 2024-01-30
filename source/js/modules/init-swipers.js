2f3e7d9e3d8450f6a5b08b44e76a9a93ad43670a
booblik2022 <booblik2022@gmail.com>
2024-01-30 12:39:33 +0300

    Добваил модуль управления всеми свайперами

---
+++ source/js/modules/init-swipers.js
@@ -0,0 +1,12 @@
+import {initSliderHero} from './hero-swiper';
+import {initSliderReviews} from './reviews-swiper';
+import {initSliderNews} from './news-swiper';
+
+const initSliders = () => {
+  initSliderHero();
+  initSliderReviews();
+  initSliderNews();
+};
+
+
+export {initSliders};
