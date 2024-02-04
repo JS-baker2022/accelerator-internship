7677f38869f2c1dd227146207a0fa3809eca41d7
booblik2022 <booblik2022@gmail.com>
2024-02-04 23:15:25 +0300

    Исправление ошибок

--- source/js/modules/hero-swiper.js
+++ source/js/modules/hero-swiper.js
@@ -1,24 +1,37 @@
 import Swiper from 'swiper/bundle';
-
-// import styles bundle
+import {Pagination, Autoplay} from 'swiper/modules';
 import 'swiper/css/bundle';
 
 
 const heroSlider = document.querySelector('[data-swiper="slider"]');
 const heroSliderPagination = document.querySelector('[data-button="pagination-slider"]');
 
-const setSliderHero = () => new Swiper(heroSlider, {
+const setSliderHero = () => new Swiper(heroSlider,{
+  modules: [Pagination, Autoplay],
   pagination: {
     el: heroSliderPagination,
     clickable: true,
     type: 'bullets',
-    bulletElement: 'div',
+    bulletElement: 'span',
     bulletClass: 'hero-slider__pagination-bullet',
     bulletActiveClass: 'is-active',
+  },
+  on: {
+    slideChangeTransitionStart: function () {
+      setSliderHero.pagination.init();
+      setSliderHero.pagination.render();
+      setSliderHero.pagination.update();
+    },
   },
   loop: true,
   breakpoints: {
     1440: {
+      allowTouchMove: true,
+    },
+    768: {
+      allowTouchMove: true,
+    },
+    320: {
       allowTouchMove: true,
     },
   },
